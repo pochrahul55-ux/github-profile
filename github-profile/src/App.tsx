24d17272ecb66b3486f342ed86cacbcc65e63451
Rahul Pochaboina <Rahul.Pochaboina.Contractor@pepsico.com>
2026-05-18 13:51:24 -0400

    Add ProfileHeader and ProfileInfo components to App

--- github-profile/src/App.tsx
+++ github-profile/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import SearchBar from './components/Hero'
+import ProfileHeader from './components/ProfileHeader'
+import ProfileInfo from './components/ProfileInfo'
 
 function App() {
   return (
     <>
       <SearchBar />
+      <ProfileHeader />
+      <ProfileInfo />
     </>
   )
 }
