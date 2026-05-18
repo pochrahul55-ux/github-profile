7546733f2aee916c96d2e3f10f21c7993579be8a
Rahul Pochaboina <Rahul.Pochaboina.Contractor@pepsico.com>
2026-05-18 13:51:06 -0400

    Add ProfileInfo component with styles and structure

---
+++ github-profile/src/components/ProfileInfo.tsx
@@ -0,0 +1,13 @@
+import './ProfileInfo.css';
+
+function ProfileInfo() {
+  return (
+    <>
+      <div className="profile-info">
+        <h1 className="profile-name">GitHub</h1>
+        <p className="profile-bio">How people build software.</p>
+      </div>
+    </>
+  )
+}
+export default ProfileInfo;
